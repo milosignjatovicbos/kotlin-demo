f24d55031ee5387d2092dd879dbc70830971b9e0
Jakub Petrzilka <jakub.petrzilka@dossier.no>
2019-10-19 22:44:24 +0200

    WIP

--- common/src/commonMain/kotlin/no/dossier/app/kotlindemo/model/User.kt
+++ common/src/commonMain/kotlin/no/dossier/app/kotlindemo/model/User.kt
@@ -4,8 +4,8 @@ import kotlinx.serialization.Serializable
 import no.dossier.app.kotlindemo.utils.getPlatformName
 
 @Serializable
-data class User(val name: String, val surname: String) {
+data class User(val name: String, val surname: String, val createdOn: String = getPlatformName()) {
 
     val formattedName: String
-        get() = "$name $surname (instance in ${getPlatformName()})"
+        get() = "$name $surname (created on $createdOn)"
 }
