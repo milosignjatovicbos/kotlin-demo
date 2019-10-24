b7bdc1b3a2d5aa8eab78de1450b7c0cac047c984
Jakub Petrzilka <jakub.petrzilka@dossier.no>
2019-10-24 07:13:27 +0200

    Fixed - Missing user file

---
+++ common/src/commonMain/kotlin/no/dossier/app/kotlindemo/domain/User.kt
@@ -0,0 +1,11 @@
+package no.dossier.app.kotlindemo.domain
+
+import kotlinx.serialization.Serializable
+import no.dossier.app.kotlindemo.utils.getPlatformName
+
+@Serializable
+data class User(val name: String, val surname: String, val createdOn: String = getPlatformName()) {
+
+    val formattedName: String
+        get() = "$name $surname (created on $createdOn)"
+}
